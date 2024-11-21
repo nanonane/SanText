a88c7d0aae3e929fd91ba60b26ceec8a2e6dbebb
Yinuo Zhu <yinuo-zhu@outlook.com>
2024-11-21 16:39:14 +0800

    update paths

--- query.sh
+++ query.sh
@@ -2,11 +2,11 @@
 python run_SanText.py \
 --task PB \
 --method SanText_plus \
---epsilon 2.0 \
+--epsilon 0.2 \
 --word_embedding_path ./data/glove.840B.300d.txt \
 --word_embedding_size 300 \
---data_dir ../../PAPB/USPB/ForUsers/qOnly \
---output_dir ./output_SanText_plus_glove/PB/ \
+--data_dir ../EnochPB/USPB/ForUsers/qOnly \
+--output_dir ./output_SanText_plus_glove/test/ \
 --threads 12 \
 --p 0.3 \
 --sensitive_word_percentage 0.9
